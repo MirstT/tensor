c419a13865b36949e5ee2951ce49aa7d2ee46e57
Mirst <Mirst@outlook.com>
2023-08-24 19:23:14 +0800

    feat(std_array): std array -> fixed shape matrix

---
+++ tensor_std_array/tensor_std_array.cc
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<array>
+using namespace std;
+
+int main() {
+	std::array<std::array<int, 2>, 3> matrix1 = { 1,3,1,0,1,2 };
+	std::array<std::array<int, 2>, 3> matrix2 = { 0,0,7,5,2,1 };
+	cout << "第一个矩阵为" << endl;
+	for (int i = 0; i <= 2; i++) {
+		for (int j = 0; j <= 1; j++) {
+			cout << matrix1[i][j] << "  ";
+			if (j == 1) {
+				cout << endl;
+			}
+		}
+	}
+	cout << "第二个矩阵为" << endl;
+	for (int i = 0; i <= 2; i++) {
+		for (int j = 0; j <= 1; j++) {
+			cout << matrix2[i][j] << "  ";
+			if (j == 1) {
+				cout << endl;
+			}
+		}
+	}
+	cout << "相加后的矩阵为" << endl;
+	for (int i = 0; i <= 2; i++) {
+		for (int j = 0; j <= 1; j++) {
+			cout << matrix1[i][j] + matrix2[i][j] << "  ";
+			if (j == 1) {
+				cout << endl;
+			}
+		}
+	}
+}
