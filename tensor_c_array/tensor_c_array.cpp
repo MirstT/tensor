7fef12ca40d754023e0f40503de779c82f121fd7
Mirst <Mirst@outlook.com>
2023-08-24 20:28:05 +0800

    chore(array): raise questions related to fixed shape matrix

--- tensor_c_array/tensor_c_array.cpp
+++ tensor_c_array/tensor_c_array.cpp
@@ -1,76 +1,43 @@
-﻿#define _CRT_SECURE_NO_WARNINGS 1 
-#include<stdio.h>
+﻿//------------------------------------------------------
+//  禁止使用 std::array
+//  禁止使用与堆区内存相关的操作，比如 malloc,new,std::vector等
+//  禁止使用与全局区内存相关的操作，比如 全局变量,static 等
+//------------------------------------------------------
+#include<iostream>
 int main()
 {
-	int i, j;
-	int a[3][2];
+	constexpr int col{ 2 };
+	constexpr int row{ 3 };
 
+	int matrix1[row][col]{};
+	int matrix2[row][col]{};
+	int matrix_sum[row][col]{};
 
-	int b[3][2];
-	int c[3][2];
-	printf("规定两个3行2列矩阵相加\n\n");
+	std::cout << "第一个矩阵为\n";
+	PopulateRandomDataByPointer(&matrix1);
+	//PopulateRandomDataByReference(matrix1);
+	Print(matrix1);
 
-	printf("请输入第一个矩阵的元素：\n");
-	for (i = 0; i <= 2; i++)
-	{
-		for (j = 0; j <= 1; j++)
-		{
-			scanf("%d", &a[i][j]);
-		}
-	}
-	printf("\n");
+	std::cout << "第二个矩阵为\n";
+	PopulateRandomDataByPointer(&matrix2);
+	//PopulateRandomDataByReference(matrix2);
+	Print(matrix2);
 
-	printf("第1个矩阵为:\n");
-	for (i = 0; i <= 2; i++)
-	{
-		for (j = 0; j <= 1; j++)
-		{
-			printf("%d  ", a[i][j]);
-			if (j == 1)
-			{
-				printf("\n");
-			}
-		}
-	}
-	printf("\n");
-
-	printf("请输入第二个矩阵的元素：\n");
-	for (i = 0; i <= 2; i++)
-	{
-		for (j = 0; j <= 1; j++)
-		{
-			scanf("%d", &b[i][j]);
-		}
-	}
-	printf("\n");
-
-	printf("第2个矩阵为:\n");
-	for (i = 0; i <= 2; i++)
-	{
-		for (j = 0; j <= 1; j++)
-		{
-			printf("%d  ", b[i][j]);
-			if (j == 1)
-			{
-				printf("\n");
-			}
-		}
-	}
-	printf("\n");
-
-
-	printf("这两个矩阵相加的结果如下:\n");
-	for (i = 0; i <= 2; i++)
-	{
-		for (j = 0; j <= 1; j++)
-		{
-			c[i][j] = a[i][j] + b[i][j];
-			printf("%d  ", c[i][j]);
-			if (j == 1)
-			{
-				printf("\n");
-			}
-		}
-	}
-	return 0;
+	std::cout << "相加后的矩阵为\n";
+	AddByPointer(&matrix_sum, &matrix1, &matrix2);
+	//AddByReference(matrix_sum, matrix1, matrix2);
+	Print();
 }
+
+//matrix1 = PopulateRandomDataByValue(matrix1); 
+//PopulateRandomDataByValue() {}; 为什么c原生数组在“上述条件限制下”不能使用这种方式获取返回值？
+PopulateRandomDataByPointer() {};
+PopulateRandomDataByReference() {};
+
+AddByValue() {};
+AddByPointer() {};
+AddByReference() {};
+
+Print() {};
+
+
--- tensor_std_array/tensor_std_array.cc
+++ tensor_std_array/tensor_std_array.cc
@@ -1,35 +1,44 @@
-﻿#include<iostream>
+﻿//------------------------------------------------------
+//  禁止使用与堆区内存相关的操作，比如 malloc,new,std::vector等
+//  禁止使用与全局区内存相关的操作，比如 全局变量,static 等
+//------------------------------------------------------
+#include<iostream>
 #include<array>
-using namespace std;
 
 int main() {
-	std::array<std::array<int, 2>, 3> matrix1 = { 1,3,1,0,1,2 };
-	std::array<std::array<int, 2>, 3> matrix2 = { 0,0,7,5,2,1 };
-	cout << "第一个矩阵为" << endl;
-	for (int i = 0; i <= 2; i++) {
-		for (int j = 0; j <= 1; j++) {
-			cout << matrix1[i][j] << "  ";
-			if (j == 1) {
-				cout << endl;
-			}
-		}
-	}
-	cout << "第二个矩阵为" << endl;
-	for (int i = 0; i <= 2; i++) {
-		for (int j = 0; j <= 1; j++) {
-			cout << matrix2[i][j] << "  ";
-			if (j == 1) {
-				cout << endl;
-			}
-		}
-	}
-	cout << "相加后的矩阵为" << endl;
-	for (int i = 0; i <= 2; i++) {
-		for (int j = 0; j <= 1; j++) {
-			cout << matrix1[i][j] + matrix2[i][j] << "  ";
-			if (j == 1) {
-				cout << endl;
-			}
-		}
-	}
+	constexpr int col{ 2 };
+	constexpr int row{ 3 };
+
+	std::array<std::array<int, col>, row> matrix1{};
+	std::array<std::array<int, col>, row> matrix2{};
+	std::array<std::array<int, col>, row> matrix_sum{};
+
+
+	std::cout << "第一个矩阵为\n";
+	matrix1 = PopulateRandomDataByValue(matrix1);
+	//PopulateRandomDataByPointer(&matrix1);
+	//PopulateRandomDataByReference(matrix1);
+	Print(matrix1);
+
+	std::cout << "第二个矩阵为\n";
+	matrix2 = PopulateRandomDataByValue(matrix2);
+	//PopulateRandomDataByPointer(&matrix2);
+	//PopulateRandomDataByReference(matrix2);
+	Print(matrix2);
+
+	std::cout << "相加后的矩阵为\n";
+	matrix_sum = AddByValue(matrix1, matrix2);
+	//AddByPointer(&matrix_sum, &matrix1, &matrix2);
+	//AddByReference(matrix_sum, matrix1, matrix2);
+	Print();
 }
+
+PopulateRandomDataByValue() {};
+PopulateRandomDataByPointer() {};
+PopulateRandomDataByReference() {};
+
+AddByValue() {};
+AddByPointer() {};
+AddByReference() {};
+
+Print() {};
