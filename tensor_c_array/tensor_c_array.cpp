762688f5f1070d76796387d4588a7f90d96d43bb
Mirst <Mirst@outlook.com>
2023-08-24 20:35:42 +0800

    refactor(array): constexpr & using

--- tensor_c_array/tensor_c_array.cpp
+++ tensor_c_array/tensor_c_array.cpp
@@ -1,17 +1,18 @@
 ﻿//------------------------------------------------------
 //  禁止使用 std::array
 //  禁止使用与堆区内存相关的操作，比如 malloc,new,std::vector等
-//  禁止使用与全局区内存相关的操作，比如 全局变量,static 等
+//  禁止新增与全局区内存相关的操作，比如 全局变量,static 等
 //------------------------------------------------------
 #include<iostream>
-int main()
-{
-	constexpr int col{ 2 };
-	constexpr int row{ 3 };
 
-	int matrix1[row][col]{};
-	int matrix2[row][col]{};
-	int matrix_sum[row][col]{};
+constexpr int kCol{ 2 };
+constexpr int kRow{ 3 };
+
+int main() {
+
+	int matrix1[kRow][kCol]{};
+	int matrix2[kRow][kCol]{};
+	int matrix_sum[kRow][kCol]{};
 
 	std::cout << "第一个矩阵为\n";
 	PopulateRandomDataByPointer(&matrix1);
--- tensor_std_array/tensor_std_array.cc
+++ tensor_std_array/tensor_std_array.cc
@@ -1,17 +1,20 @@
 ﻿//------------------------------------------------------
 //  禁止使用与堆区内存相关的操作，比如 malloc,new,std::vector等
-//  禁止使用与全局区内存相关的操作，比如 全局变量,static 等
+//  禁止新增与全局区内存相关的操作，比如 全局变量,static 等
 //------------------------------------------------------
 #include<iostream>
 #include<array>
 
-int main() {
-	constexpr int col{ 2 };
-	constexpr int row{ 3 };
+constexpr int kCol{ 2 };
+constexpr int kRow{ 3 };
+using Matrix = std::array<std::array<int, kCol>, kRow>;
 
-	std::array<std::array<int, col>, row> matrix1{};
-	std::array<std::array<int, col>, row> matrix2{};
-	std::array<std::array<int, col>, row> matrix_sum{};
+int main() {
+
+
+	Matrix matrix1{};
+	Matrix matrix2{};
+	Matrix matrix_sum{};
 
 
 	std::cout << "第一个矩阵为\n";
