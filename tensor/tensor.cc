1b2e9d40d11005962ecad8b4933c41bb9b9f0ed3
Mirst <Mirst@outlook.com>
2023-08-24 10:40:51 +0800

    feat(tensor): tensor

--- tensor/tensor.cc
+++ tensor/tensor.cc
@@ -1,39 +1,58 @@
 ﻿#include <iostream>
 #include <vector>
 using namespace std;
-#include <stdlib.h>            //程序中用到了malloc函数和随机数函数，应包含stdlib.h
+#include <stdlib.h>
 #include <time.h>
 
-vector<int> getMatrixSize(int size) {
+
+vector<int> getRandMatrixByShape(vector<int> shape) {
+
+	/*定义得到矩阵所有元素的个数的函数，（所有元素的个数通
+	过矩阵各个维度的大小相乘来得到*/
+
 	vector<int> matrix;
-	for (int i = 0; i < size; i++) {
-		int t = rand() % 100;
-		matrix.push_back(t);
+	int matrixSize = 1;
+	for (unsigned int i = 0; i < shape.size(); i++) { //将i定义为无符号整形的变量，否则编译器会报错
+		matrixSize *= shape[i];
+	}
+	for (int j = 0; j < matrixSize; j++) {
+		int randomData = rand() % 100;
+		matrix.push_back(randomData);
 	}
 	return matrix;
 }
 
-vector<int> matrixAdd(vector<int> matrix1, vector<int> matrix2) {
-	vector<int> matrixsum(matrix1.size());            //matrixsum的意思是矩阵相加后的总和
-	for (int i = 0; i < matrix1.size(); i++) {
-		matrixsum[i] = matrix1[i] + matrix2[i];
+vector<int> addMatrix(vector<int> matrix1, vector<int> matrix2) {
+	vector<int> matrixSum(matrix1.size());
+	for (unsigned int i = 0; i < matrix1.size(); i++) {
+		matrixSum[i] = matrix1[i] + matrix2[i];
 	}
-	return matrixsum;
+	return matrixSum;
 }
 
 void printMatrix(string matrixName) {
-	cout << "------| " << matrixName << " |------" << endl;
+	cout << "-------------------------------| " << matrixName << " |-------------------------------" << endl;
 }
 
-void printMatrixShape(vector<int> matrix, int m, int n) {
+void printMatrix(vector<int> matrix, vector<int> shape) {
 	printMatrix("Matrix");
-	for (int i = 1; i <= matrix.size(); i++) {
+	for (unsigned int i = 1; i <= matrix.size(); i++) {
 		cout << matrix[i - 1] << "  ";
-		if (i % n == 0) {                    //打印出三维矩阵中二维矩阵的形状
-			cout << "\n";
-		}
-		if (i % (m * n) == 0) {                  //打印出三维矩阵的形状
-			cout << "\n";
+		int t = shape[shape.size() - 1];
+		for (unsigned int j = shape.size(); j >= 2; j--) {
+			if (i % t == 0) {
+				cout << "\n";
+				if (j <= shape.size() - 1) {
+					printf("~~~|%d维分界线|~~~\n", shape.size() - j + 2);
+
+					/*举例说明：在一个三维矩阵中因为多个二维矩阵可以直接在屏幕上打印出来，中间也只需要
+					换行符标明多个二维矩阵的界限，但三维矩阵是由多个二维矩阵组成，如果只用换行符表明，
+					那么三维的大小也很难在屏幕上看出来，所以最后加个某维的分界线来表明矩阵各个维度的大小
+					这个地方的代码也许有些乱和复杂，但最终确实能实现打印各个维度分界线的效果*/
+
+				}
+				t *= (shape[j - 2]);
+			}
 		}
 	}
 }
@@ -41,34 +60,40 @@ void printMatrixShape(vector<int> matrix, int m, int n) {
 int judgeCin() {
 	int a;
 	cin >> a;
-	while (a <= 0 || a >= 11) {                  //判断输入的列数是否有误
+	while (a <= 1 || a >= 11) { //判断输入的列数是否有误
 		cout << "输入有错误，请重新输入新的取值=";
-		fflush(stdin);                      //清空输入缓冲区 
+		fflush(stdin); //清空输入缓冲区
 		cin >> a;
 	}
 	return a;
 }
 
+vector<int> getMatrixDimension() { //定义得到矩阵各个维度大小的函数
+	int d = judgeCin();
+	vector<int> dimension;
+	for (int i = d; i >= 1; i--) {
+		printf("请输入第%d维度的大小=", i);
+		int t = judgeCin();
+		dimension.push_back(t);
+	}
+	return dimension;
+}
+
 int main() {
 
 	srand((unsigned)time(NULL));
-	cout << "定两个l层m行n列的矩阵相加(注意：l和m和n的取值范围是1至10.)\n请输入层数l=";
-	int l = judgeCin();
-	cout << "请输入行数n=";
-	int m = judgeCin();
-	cout << "请输入列数n=";
-	int n = judgeCin();
-	int size = l * m * n;
+	cout << "规定两个n维矩阵相加(注意：所有需要输入的数的取值范围是1至10.)\n请输入矩阵维度的大小d=";
+	vector<int> shape = getMatrixDimension();
 
-	vector<int> matrix1 = getMatrixSize(size);
-	printMatrixShape(matrix1, m, n);
+	vector<int> matrix1 = getRandMatrixByShape(shape);
+	printMatrix(matrix1, shape);
 
-	vector<int> matrix2 = getMatrixSize(size);
-	printMatrixShape(matrix2, m, n);
+	vector<int> matrix2 = getRandMatrixByShape(shape);
+	printMatrix(matrix2, shape);
 
 	cout << "\n相加后的矩阵为：" << endl;
-	vector<int> matrixsum = matrixAdd(matrix1, matrix2);
-	printMatrixShape(matrixsum, m, n);
+	vector<int> matrixSum = addMatrix(matrix1, matrix2);
+	printMatrix(matrixSum, shape);
 
-	return 0;
+	return -1;
 }
